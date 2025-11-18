fab958b2070be63f2fc006b3106fd6c9eefa4c81
karan pun <karanpun011@gmail.com>
2025-11-18 16:06:37 -0500

    add cards

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,28 @@
 import DateRow from '@/components/date-picker';
 import { CommandMenu } from '../../components/command-menu';
+import GameCard from '../ui/game-card';
 
 const Dashboard = () => {
     return (
-        <div className="flex flex-col items-center gap-4 ">
-            <CommandMenu />
-            <DateRow />
-        </div>
+            <>
+                <div className="flex flex-col items-center gap-4 ">
+                    <CommandMenu />
+                    <DateRow />
+                </div>
+                <div className='bg-muted mt-4 h-screen rounded-lg'>
+                    {/* <div className='flex flex-row py-3 text-accent-foreground text-sm rounded-lg '>
+                        <p className='ml-3'>Time</p>
+                        <p className='ml-14'>Match</p>
+                        <p className='ml-18'>AI Tips</p>
+                        <p className='ml-7'>Trust</p>
+                        
+                    </div> */}
+                    <hr />
+                    <div>
+                        <GameCard />
+                    </div>
+                </div>
+            </>
     );
 };
 
